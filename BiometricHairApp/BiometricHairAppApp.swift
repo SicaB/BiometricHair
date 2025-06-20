0e4856f18c4bdab0b6081b4c95497032102a8e13
Sacha Behrend <sacha.behrend@gmail.com>
2025-06-20 11:26:53 +0200

    Initial Commit

---
+++ BiometricHairApp/BiometricHairAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricHairAppApp.swift
+//  BiometricHairApp
+//
+//  Created by Admin on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BiometricHairAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
